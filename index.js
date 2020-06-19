eb29eb04c2bd9e54263008ea3c47bae14bf27623
ArisyNeko <48942274+ArisyNeko@users.noreply.github.com>
2020-06-19 20:37:08 +0200

    calculating bombs

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Animal = (function () {
     function Animal() {
+        this.clicks = 0;
+        this.first = true;
         this.things = [];
         for (var i = 0; i <= 4; i++) {
             this.things[i] = [];
@@ -13,17 +15,45 @@ var Animal = (function () {
             }
         }
     }
-    Animal.prototype.handleClick = function (row, column) {
+    Animal.prototype.showGrid = function () {
         console.log(this.things);
-        if (this.things[row][column] == true) {
-            console.log("you lose");
+    };
+    Animal.prototype.handleClick = function (row, column) {
+        this.clicks++;
+        console.log("clicks: " + this.clicks);
+        if (this.clicks != 1) {
+            if (this.things[row][column] == true) {
+                console.log("you lose");
+            }
+            else {
+                var quantity = this.countBombsAround(row, column);
+                console.log("quantity: " + quantity);
+            }
         }
+        else {
+            this.things[row][column] = false;
+            var quantity = this.countBombsAround(row, column);
+            console.log("quantity: " + quantity);
+        }
+    };
+    Animal.prototype.countBombsAround = function (row, column) {
+        var quantity = 0;
+        for (var i = -1; i < 2; i++) {
+            for (var j = -1; j < 2; j++) {
+                if (this.things[row + i] && this.things[row + i][column + j] == true) {
+                    quantity++;
+                }
+            }
+        }
+        return quantity;
     };
     return Animal;
 })();
 var dog = new Animal();
-function ChangeCellBoolean(id) {
+dog.showGrid();
+function takeInfo(id) {
     var element = document.getElementById(id.toString());
-    console.log(element);
+    //беру целый елемент див, ниже подтверджение что это такое
+    //console.log(element)
     dog.handleClick(Number(element.getAttribute("row")), Number(element.getAttribute("column")));
 }
--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 class Animal {
+    clicks: number = 0;
+    first: boolean = true;
     public things: boolean[][];
     constructor() {
         this.things =[];
@@ -15,26 +17,56 @@ class Animal {
             } 
         }
     }
-    
-    
-    handleClick(row:number, column : number) {
+    showGrid(){
         console.log(this.things);
-        
-        if(this.things[row][column]==true){
-            console.log("you lose");
+    }
+
+    handleClick(row:number, column:number) {
+
+        this.clicks++;
+        console.log("clicks: "+this.clicks);
+
+
+        if(this.clicks != 1){
+            if(this.things[row][column]==true){
+                console.log("you lose");
+            }else{
+                let quantity = this.countBombsAround(row,column);
+                console.log("quantity: "+quantity);
+            }
+        }else{
+            this.things[row][column]=false;
+
+            let quantity = this.countBombsAround(row,column);
+            console.log("quantity: "+quantity);
         }
     }
 
+    countBombsAround(row:number, column:number){
+        let quantity:number = 0;
+        for(var i:number = -1; i < 2; i++){
+            for(var j:number = -1; j < 2; j++){
+                
+                if(this.things[row+i]&&this.things[row+i][column+j]==true){
+                    quantity++;
+                }
+            }
+        }
+        return quantity;
+    }
+    
 }
 
 
 let dog = new Animal();
+dog.showGrid();
 
 
-function ChangeCellBoolean (id : number)
+function takeInfo (id:number)
 {
     let element = document.getElementById(id.toString());
-    console.log(element)
+    //беру целый елемент див, ниже подтверджение что это такое
+    //console.log(element)
 
-    dog.handleClick( Number(element.getAttribute("row")), Number(element.getAttribute("column")));
+    dog.handleClick(Number(element.getAttribute("row")), Number(element.getAttribute("column")));
 }
